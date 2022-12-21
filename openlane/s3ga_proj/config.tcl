ffdcd54be10350735be309a63e3c357b157ec593
Jan Gray <jan@fpga.org>
2022-12-20 16:55:15 -0800

    add openlane/s3ga_proj

---
+++ openlane/s3ga_proj/config.tcl
@@ -0,0 +1,25 @@
+# User config
+set ::env(DESIGN_NAME) s3ga_proj
+set ::env(ROUTING_CORES) 4
+
+# Change if needed
+set ::env(VERILOG_INCLUDE_DIRS) "$::env(DESIGN_DIR)"
+set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/../../verilog/rtl/s3ga/src/*.h $::env(DESIGN_DIR)/../../verilog/rtl/s3ga/src/*.v]
+
+# Fill this
+set ::env(CLOCK_PERIOD) "8"
+set ::env(CLOCK_PORT) "clk"
+set ::env(FP_SIZING) absolute
+set ::env(DIE_AREA) "0 0 1000 1000"
+set ::env(DESIGN_IS_CORE) 0
+set ::env(RT_MAX_LAYER) {met4}
+set ::env(VDD_NETS) [list {vccd1}]
+set ::env(GND_NETS) [list {vssd1}]
+
+set ::env(FP_PIN_ORDER_CFG) $::env(DESIGN_DIR)/pin_order.cfg
+
+set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
+if { [file exists $filename] == 1} {
+	source $filename
+}
+
